0bfc6bbc38de99e745458cb0801436ac1d778c7b
Youlong Li <chongzizil@gmail.com>
2014-10-24 00:52:27 -0400

    Update

--- src/com/li/zil/DPSqrt.java
+++ src/com/li/zil/DPSqrt.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DPSqrt {
-	int n;
+	int num;
 
-	public int getNumber(int n) {
-		this.n = n;
+	public int getNumber(int num) {
+		this.num = num;
 
 		return solution();
 	}
 
 	private int solution() {
-		int res = 0;
-		int maxSqrt = (int) Math.sqrt(n);
-		int[][] f = new int[maxSqrt + 1][n + 1];
-		int[] res2 = new int[maxSqrt + 1];
+		int max = (int) Math.sqrt(num);
+		int[][] f = new int[max + 1][num + 1];
+		int[][] s = new int[max + 1][num + 1];
+		List<Integer> res = new ArrayList<Integer>();
 
-		for (int i = 0; i <= maxSqrt; i++) {
-			for (int j = 0; j <= n; j++) {
+		for (int i = 0; i <= max; i++) {
+			for (int j = 0; j <= num; j++) {
 				if (i == 0) {
 					if (j == 0) {
 						f[i][j] = 0;
@@ -27,22 +27,24 @@ public class DPSqrt {
 						f[i][j] = Integer.MAX_VALUE;
 					}
 				} else {
-					for (int k = 0; k <= j / (i * i); k++) {
-						f[i][j] = Math.min(f[i - 1][j], f[i - 1][j - k * (i * i)] + k);
+					for (int k = 0; k * i * i <= j; k++) {
+						if (f[i][j] > f[i - 1][j - k * i * i] + k) {
+							f[i][j] = f[i - 1][j - k * i * i] + k;
+							s[i][j] = k;
+						}
 					}
 				}
 			}
 		}
 
-		List<Integer> resList = new ArrayList<Integer>();
+		int j = num;
+		for (int i = max; i >= 0; i--) {
+			for (int k = 0; k < f[i][j]; k++) {
+				res.add(i);
+				j -= i * i;
+			}
+		}
 
-		res = f[maxSqrt][n];
-
-		return res;
+		return f[max][num];
 	}
-
-//	private List<Integer> getResList(List<Integer> res) {
-//
-//	}
-
 }
