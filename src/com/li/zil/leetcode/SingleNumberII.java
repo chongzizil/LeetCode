6302b652e9f0db2d5bd6f6bc333d8fabdce925bc
Youlong Li <chongzizil@gmail.com>
2014-10-29 21:06:45 -0400

    update

--- src/com/li/zil/leetcode/SingleNumberII.java
+++ src/com/li/zil/leetcode/SingleNumberII.java
@@ -16,6 +16,19 @@ public class SingleNumberII {
 		return result;
 	}
 
+	// https://oj.leetcode.com/discuss/6632/challenge-me-thx
+	// Another neat solution, and much more easy to understand...
+	// It use two bit to store the number of times of 1 appears in
+	// the array. (ones, twos: 00 -> 10 -> 01 -> 00)
+	public int singleNumberB(int[] A) {
+		int ones = 0, twos = 0;
+		for(int i = 0; i < A.length; i++){
+			ones = (ones ^ A[i]) & ~twos;
+			twos = (twos ^ A[i]) & ~ones;
+		}
+		return ones;
+	}
+
 	//TODO: One very neat solution, still need time to figure it out.
 //	public int singleNumber(int[] A) {
 //		int ones = 0, twos = 0, threes = 0;
